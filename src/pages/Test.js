782735c67ccc5ddb0d85e166f80d5f19d16237f1
Adrian Marzo <amarzo6e@gmail.com>
2023-02-24 19:08:58 +0100

    Dificultad movil

---
+++ src/pages/Test.js
@@ -0,0 +1,21 @@
+import Footer from '../Componentes/Footer';
+import Header from '../Componentes/Header';
+import Navbar from '../Componentes/Navbar';
+
+function Test() {
+    return (
+        <div className="categoriasContainer">
+            <header>
+                <Header></Header>
+            </header>
+            <main className="mainTest">
+                
+            </main>
+            <Navbar></Navbar>
+            <footer>
+                <Footer></Footer>
+            </footer>
+        </div>
+
+    );
+} export default Test;
